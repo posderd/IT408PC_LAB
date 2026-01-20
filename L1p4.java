88c479e29a8ee1d0cf559cfda516a38e0d8330e6
posderd <rohanoddam@gmail.com>
2026-01-20 16:29:12 +0530

    Add files via upload

---
+++ L1p4.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class L1p4{
+		public static void main(String[] args){
+			int a =1;
+			double b = 2.22;
+			char d ='h';
+			String s =  "ssess";
+			boolean w = true;
+			float f  =  5533.545555f; 
+			System.out.println("int = " + a + "\ndouble = " +b + "\n float =  "+ f + "\nchar = " + d + "\nstring = " + s + "\n boolean =  " + w);
+		}
+}
---
+++ three.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+class three{
+	public static void main(String[] args){
+		int x =50;
+		System.out.println("Int value" + x );
+		double y = x;
+		System.out.println("double  value " +y);
+		double z = 82.33;
+		System.out.println("double"+z);
+		int v = (int) z;
+		System.out.println("int" + v);
+}
+}
