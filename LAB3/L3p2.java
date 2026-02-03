1ab18870cb5a64e186cd9aef07ec9932314262f0
posderd <rohanoddam@gmail.com>
2026-02-03 14:51:46 +0530

    Add L3p2 class extending L3p1 with methods

---
+++ LAB3/L3p2.java
@@ -0,0 +1,15 @@
+class L3p2 extends L3p1{
+	static int neu = 20;
+	
+	public static void display(int oop){
+	System.out.println(view+neu+oop);
+	}
+	
+	public int show(int pop){
+	return pop+view+neu;
+	}
+	
+	public void pearl(){
+	System.out.println("Iam child of L3p1");
+	}
+}
