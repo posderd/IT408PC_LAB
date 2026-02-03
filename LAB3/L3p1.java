45cb47511371dcfed16fd9422a1201433ae176ae
posderd <rohanoddam@gmail.com>
2026-02-03 14:50:41 +0530

    Add L3p1 class with display and show methods

---
+++ LAB3/L3p1.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+public class L3p1{
+	static int  view = 30; 
+	
+	public static  void display(){
+	System.out.println(view);
+	}
+	
+	public int show(){
+	return view;
+	}
+}
