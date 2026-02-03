c39ff7b98f31782d17fb2c375bb3ab26434b6d5b
posderd <rohanoddam@gmail.com>
2026-02-03 14:53:09 +0530

    Add L3p3.java with main method and class instances

---
+++ LAB3/L3p3.java
@@ -0,0 +1,15 @@
+class L3p3 {
+	public static void main(String[] args){
+	 
+	 L3p1 a = new L3p1();
+	 a.display();
+	 int n = a.show();
+	 System.out.println(n);
+	 System.out.println();
+	 L3p2 b = new L3p2();
+	 b.display(10);
+	 int s = b.show(10);
+	 System.out.println(s);
+	 b.pearl();
+	 }
+}
